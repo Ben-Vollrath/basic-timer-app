af25071607fcd881ed55719f1187628a3b4f7294
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-03-31 16:18:43 +0200

    Added default flutter project

---
+++ basic_timer_app/README.md
@@ -0,0 +1,3 @@
+# basic_timer_app
+
+A new Flutter project.
---
+++ basic_timer_app/android/app/src/main/kotlin/com/example/basic_timer_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.basic_timer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
