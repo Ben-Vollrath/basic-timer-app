12bbf4cccb354a5d4b548181a0586aa99325cddf
Ben-Vollrath <ben.m.vollrath@gmail.com>
2024-04-08 13:56:34 +0200

    Added new file to extract timer widget to

---
+++ lib/timer/widgets/interactive_timer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InteractiveTimer extends StatefulWidget {
+  const InteractiveTimer({super.key});
+
+  @override
+  State<InteractiveTimer> createState() => _InteractiveTimerState();
+}
+
+class _InteractiveTimerState extends State<InteractiveTimer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
