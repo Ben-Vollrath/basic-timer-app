a5db2efb0f6724265fc4ea1a519b7cad0d242bbc
Ben Vollrath <ben.m.vollrath@gmail.com>
2024-04-10 23:23:14 +0200

    Improved start timer animation

--- lib/static/colors.dart
+++ lib/static/colors.dart
@@ -1,8 +1,10 @@
 import 'package:basic_timer_app/utils.dart';
+import 'package:flutter/material.dart';
 
 class AppColors {
   static var backgroundWhite = HexColor('#FDFDF4');
   static var mainOrange = HexColor('#F6A881');
   static var mainBlue = HexColor('#6DCFFF');
   static var mainPurple = HexColor('#A177B0');
+  static var black = Colors.black;
 }
--- lib/timer/widgets/interactive_timer.dart
+++ lib/timer/widgets/interactive_timer.dart
@@ -1,8 +1,12 @@
-import 'package:basic_timer_app/static/colors.dart';
-import 'package:basic_timer_app/timer/widgets/filling_container.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:basic_timer_app/static/colors.dart';
+import 'package:basic_timer_app/timer/providers/start_timer_button_animation.dart';
+import 'package:basic_timer_app/timer/widgets/filling_container.dart';
 import 'package:basic_timer_app/timer/domain/services/countdown_timer_service.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 import 'package:basic_timer_app/timer/widgets/slider_style.dart';
 import 'package:basic_timer_app/timer/providers/reset_timer_animation.dart';
@@ -18,6 +22,8 @@ class _InteractiveTimerState extends State<InteractiveTimer> with TickerProvider
   final CountdownTimerService timerService = CountdownTimerService();
 
   bool buttonClicked = false;
+  ValueNotifier<double> startButtonHeight = ValueNotifier(250.00);
+  ValueNotifier<double> buttonOpacity = ValueNotifier(1.0);
 
   double selectedSeconds = 00;
   double selectedMinutes = 00;
@@ -28,23 +34,29 @@ class _InteractiveTimerState extends State<InteractiveTimer> with TickerProvider
   ValueNotifier<double> currentHours = ValueNotifier(00);
 
   late ResetTimerAnimation resetTimerAnimation;
+  late startTimerButtonAnimation startimerButtonAnimation;
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
-    return Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Align(
-            alignment: Alignment.center,
-            child: timer(), // Centered vertically in the stack.
-          ),
-          Positioned(
-            top: screenHeight / 2 + 250, // Adjust the value to position startButton() right below timer()
-            child: animatedStartButton(),
-          ),
-        ],
-      );
+    return ValueListenableBuilder(
+      valueListenable: startButtonHeight, //Placeholder
+      builder: (context, value, child) {
+      return Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.center,
+              child: timer(), // Centered vertically in the stack.
+            ),
+            Positioned(
+              top: screenHeight / 2 + value, // Adjust the value to position startButton() right below timer()
+              child: animatedStartButton(),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -55,6 +67,12 @@ class _InteractiveTimerState extends State<InteractiveTimer> with TickerProvider
       currentSeconds: currentSeconds, 
       currentMinutes: currentMinutes, 
       currentHours: currentHours
+      );
+
+    startimerButtonAnimation = startTimerButtonAnimation(
+      vsync: this,
+      startButtonHeight: startButtonHeight,
+      opacity: buttonOpacity,
       );
   }
 
@@ -141,44 +159,62 @@ class _InteractiveTimerState extends State<InteractiveTimer> with TickerProvider
           buttonClicked = !buttonClicked;
         });
         startTimer();
+        startimerButtonAnimation.moveUpController.forward();
         //TODO add stop timer functionality
       },
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 500),
-        width: buttonClicked ? 30.0 : 180.0,
-        height: buttonClicked ? 30.0 : 70.0,
-        decoration: BoxDecoration(
-          color: AppColors.mainOrange,
-          borderRadius: buttonClicked ? BorderRadius.circular(35) : BorderRadius.circular(30),
-        ),
-        child: Center(
-          child: Text(
-            buttonClicked ? '' : 'Start',
-            style: TextStyle(
-              color: AppColors.backgroundWhite,
-              fontSize: 20,
+      child: ValueListenableBuilder(
+        valueListenable: buttonOpacity,
+        builder: (context, value, child){
+          return Opacity(
+            opacity: value,
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              width: buttonClicked ? 30.0 : 180.0,
+              height: buttonClicked ? 30.0 : 70.0,
+              decoration: BoxDecoration(
+                color: AppColors.mainOrange,
+                borderRadius: buttonClicked ? BorderRadius.circular(35) : BorderRadius.circular(30),
+              ),
+              child: Center(
+                child: Text(
+                  buttonClicked ? '' : 'Start',
+                  style: TextStyle(
+                    color: AppColors.backgroundWhite,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
             ),
-          ),
-        ),
+          );
+        }
       ),
     );
   }
-  FillingContainer showTime(){
-  return FillingContainer(
-    progress: 0,
-    size: 140,
-    backgroundColor: AppColors.backgroundWhite,
-    progressColor: customColors03.trackColor!,
-    child: Center(
-      child: Text(
-          '${selectedHours.floor().toString().padLeft(2, '0')}:${selectedMinutes.floor().toString().padLeft(2, '0')}:${selectedSeconds.floor().toString().padLeft(2, '0')}',
-          style: const TextStyle(
-            fontSize: 25,
-            color: Colors.black,
+  ValueListenableBuilder showTime(){
+  return ValueListenableBuilder(
+    valueListenable: buttonOpacity,
+    builder: (context, value, child) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(140),
+          border: Border.all(
+            color: AppColors.black, 
+            width: 3
           ),
-        textAlign: TextAlign.center,
+            color: AppColors.mainOrange.withOpacity(1 - value as double), //change color
+        ),
+      child: Center(
+        child: Text(
+            '${selectedHours.floor().toString().padLeft(2, '0')}:${selectedMinutes.floor().toString().padLeft(2, '0')}:${selectedSeconds.floor().toString().padLeft(2, '0')}',
+            style: const TextStyle(
+              fontSize: 25,
+              color: Colors.black,
+            ),
+          textAlign: TextAlign.center,
+        ),
       ),
-    ),
+    );
+  },
   );
   }
 }
